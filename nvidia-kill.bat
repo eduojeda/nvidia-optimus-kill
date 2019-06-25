c78b449f505f78781a0f3a3a506aae8ee6602d0d
Eduardo Ojeda <eduojeda@gmail.com>
2019-06-25 17:24:33 +0200

    First commit

---
+++ nvidia-kill.bat
@@ -0,0 +1,1 @@
+start powershell -command "& '.\nvidia-kill.ps1'"
---
+++ nvidia-kill.ps1
@@ -0,0 +1,18 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs;
+	exit
+}
+
+$smiOutput = nvidia-smi --query-compute-apps=pid,name --format=csv
+$dgpuProcesses = $smiOutput | ConvertFrom-Csv
+echo "Running dGPU processes:"
+ForEach ($p in $dgpuProcesses) {
+    $p.process_name
+}
+
+$confirmation = Read-Host "Kill'em?"
+if ($confirmation -eq 'y') {
+    ForEach ($p in $dgpuProcesses) {
+        kill $p.pid -Force
+    }
+}
